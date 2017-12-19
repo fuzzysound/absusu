f8f175508ffb3c681d05c828e0d93112f992ab33
fuzzysound <hyun1652@gmail.com>
2017-12-19 11:01:59 +0900

    1. experimenter 디렉토리(a/b test server에 해당)와 임시적인 randomizer 추가. 이제 user의 group의 default 값은 이 randomizer에 의해 결정됨. 2. .gitignore 추가.

--- appserver_rest/models.py
+++ appserver_rest/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from experimenter.randomizer import *
 
 class UserAction(models.Model):
     user_id = models.CharField(max_length=100, blank=True, null=True)
-    group = models.CharField(max_length=100, blank=False, null=False, default='A')
+    group = models.CharField(max_length=100, blank=False, null=False, default=get_user_group(user_id))
     time = models.DateTimeField(auto_now_add=True)
     action = models.CharField(max_length=100, blank=True, null=True)
 
