eaba11e88dcf5fa84d2b71f615632d5c3ca850da
fuzzysound <hyun1652@gmail.com>
2017-12-26 10:53:33 +0900

    - models.py 디버그함

--- appserver_rest/models.py
+++ appserver_rest/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from experimenter.randomizer import get_user_group
+from experimenter_.randomizer import get_user_group
 
 class UserAction(models.Model):
     ip = models.CharField(max_length=100, blank=True, null=True)
-    group = models.CharField(max_length=100, blank=False, null=False, default=get_user_group(user_id))
+    group = models.CharField(max_length=100, blank=False, null=False, default=get_user_group(ip))
     time = models.DateTimeField(auto_now_add=True)
     action = models.CharField(max_length=100, blank=True, null=True)
 
