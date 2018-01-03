fa56a247c412be91505fa5f7c1992fec50f54d9e
fuzzysound <hyun1652@gmail.com>
2018-01-03 13:24:05 +0900

    solved circular import problem.
    - serializer의 create 메소드를 override하여 해결.

--- appserver_rest/serializers.py
+++ appserver_rest/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import UserAction
+from experimenter.randomizer import get_user_groups
 
 class UserActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAction
         fields = '__all__'
+
+    # 사용자 group 지정
+    def create(self, validated_data):
+        validated_data['groups'] = get_user_groups(validated_data['ip'])
+        return super().create(validated_data)
 
