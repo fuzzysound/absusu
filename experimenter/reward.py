d78e33d990fae5d3d994f21ea2dea9a38c007dbd
y0ngh00n <janus0807@gmail.com>
2018-01-08 16:36:36 +0900

    1) create reward.py to calculate kpi 2) implement CTR function using pyMySQL
    TODO:
    1) implement other kpi

---
+++ experimenter/reward.py
@@ -0,0 +1,38 @@
+import pymysql
+
+conn = pymysql.connect(host='localhost',
+                       user='absusu',
+                       password='absusu',
+                       db='absusu_log',
+                       charset='utf8')
+
+def CTR(experiment, group, act_subject):
+    try:
+        curs = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "select * from appserver_rest_useraction where groups = json_object('%s','%s');" % (experiment, group)
+        curs.execute(sql)
+        rows = curs.fetchall()
+
+        impressions = 0
+        clicks = 0
+        for row in rows:
+            if row['action'] == '':
+                impressions += 1
+            elif act_subject in row['action'] and 'click' in row['action']:
+                clicks += 1
+        ctr = clicks / impressions
+
+        return ctr
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+'''
+#for test
+if __name__ =="__main__":
+    result = CTR('exp1','control','button1')
+    print(result)
+'''
