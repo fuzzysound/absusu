6b9786bf15df4ff5abeb6d0e496cbb76915d506f
y0ngh00n <janus0807@gmail.com>
2018-01-15 15:29:48 +0900

    1) restore Goal models 2) test Goal models 3) modify and move reward.py 4) implement simple dashboard.py

--- absusu/dashboards.py
+++ absusu/dashboards.py
@@ -1,28 +1,19 @@
-'''
-Show AB test Dashboard
-'''
 from controlcenter import Dashboard, widgets
+from appserver_rest.models import UserAction, UserAssignment
 from experimenter.models import Experiment,Group,Goal
 from reward import KPI
 
+class UserassignmentItemList(widgets.ItemList):
+    model = UserAssignment
+    list_display = ('ip', 'assignment')
+'''
 class CTRList(widgets.ItemList):
-    '''
-    This widget displays a list of CTRs
-    '''
+    
     title = 'Click-through Rate'
-    model = Experiment
-    # multiple tables inner join queryset
-    queryset = Experiment.objects.filter(group__name__isnull=False).filter(goal__act_subject__isnull=False)\
-        .values_list('name','group__name','goal__act_subject')
-    list_display = ('name','group__name', 'goal__act_subject', 'get_ctr')
-    width = widgets.LARGE
-
-    def get_ctr(self, queryset):
-        kpi = KPI()
-        return kpi.CTR(*queryset)
-
-class AbsusuDashboard(Dashboard):
-    widgets = (
-        widgets.Group([CTRList],width = widgets.LARGER, height = 300),
-    )
-
+    model = Goal
+    list_display = ('name','track', 'act_subject', 'experiment')
+'''
+class MyDashboard(Dashboard):
+    widgets = [
+        UserassignmentItemList,
+    ]
