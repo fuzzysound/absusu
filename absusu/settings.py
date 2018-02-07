204ddff80c899c6b4bd2365775279007c7f7836d
y0ngh00n <janus0807@gmail.com>
2018-02-07 19:58:05 +0900

    1) update chartist js widget via Ajax 2) designing itemlist update..

--- absusu/settings.py
+++ absusu/settings.py
@@ -60,10 +60,10 @@ CONTROLCENTER_DASHBOARDS = (
 CONTROLCENTER_CHARTIST_COLORS = 'material'
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
